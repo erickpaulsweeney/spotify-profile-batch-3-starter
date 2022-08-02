9f48e13221d2da874d2349ecb380d37aaa5cb3d6
ErickPaulSweeney <erickpaulsweeney@gmail.com>
2022-08-02 20:47:40 +0800

    Fixed redirect uri

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -13,7 +13,7 @@ export const getToken = createAsyncThunk('auth/getToken', async (code) => {
         body: new URLSearchParams({
             'grant_type': 'authorization_code',
             'code': code,
-            'redirect_uri': 'http://localhost:3000/validate'
+            'redirect_uri': 'https://delicate-torrone-dc44fb.netlify.app/validate'
         }).toString()
     });
 
